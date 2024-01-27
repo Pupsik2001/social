ca1b85e9c33204b854b3f1c2a84c7c921b192412
Pupsik2001 <kenguru13422431@gmail.com>
2024-01-28 03:25:13 +0600

    next step change all d:>server, test

---
+++ scripts/create_dev_database.sql
@@ -0,0 +1,8 @@
+/*
+This file is used to bootstrap development database locally.
+
+Note: ONLY development database;
+*/
+
+CREATE USER "social" SUPERUSER;
+CREATE DATABASE "social" OWNER "social" ENCODING 'utf-8';
